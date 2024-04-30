ffe11f266f5ab118424c774032f8d2a650b2e4cd
Taejeong <mkiz0403@gmail.com>
2024-04-30 17:09:39 +0900

    fix & add interface.

--- client/src/models/pokemons.Interface.ts
+++ client/src/models/pokemons.Interface.ts
@@ -1,6 +1,31 @@
 export interface PokemonInterface {
+  key?: number;
   number: number;
   name: string;
   types: string[];
   imageUrl: string;
+}
+
+export interface PokemonsProps extends PokemonInterface {
+  onOnePokemonDelete: (number: number) => Promise<void>;
+  refreshPokemons: () => void;
+}
+
+export interface UpdatePokemonProps {
+  refreshPokemons: () => void;
+  onOpen: boolean; // Changed from a function to a boolean
+  onClose: () => void; // Simple function without parameters
+  pokemonData: PokemonInterface;
+}
+
+export interface CreateProps extends Pick<UpdatePokemonProps, 'onOpen' | 'onClose'> {}
+
+
+export interface ShareModalProps {
+  open: boolean;
+  onClose: () => void;
+  onConfirm: () => void;
+  title: string;
+  message: string;
+  confirmButtonText: string;
 }
