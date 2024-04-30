32a706507f8e75ed386abded87459bbaec326409
Taejeong <mkiz0403@gmail.com>
2024-04-30 17:13:45 +0900

    fix : pokemon.Schma

--- server/src/models/pokemon.Schma.ts
+++ server/src/models/pokemon.Schma.ts
@@ -1,13 +1,12 @@
 import mongoose, { Document, Schema } from 'mongoose';
 import PokemonInterface from './pokemons.Interface';
-
 interface IpokemonInterface extends Document, PokemonInterface {}
 
-const pokemonSchema: Schema = new Schema({
-  number: { type: Number, required: true },
+const pokemonSchema: Schema = new mongoose.Schema({
+  number: { type: Number, required: true, index: true },
   name: { type: String, required: true },
   types: [{ type: String, required: true }],
-  imgeUrl: { type: String, required: true },
+  imageUrl: { type: String, required: true },
 });
 
 const PokemonModel = mongoose.model<IpokemonInterface>('Pokemon', pokemonSchema);
