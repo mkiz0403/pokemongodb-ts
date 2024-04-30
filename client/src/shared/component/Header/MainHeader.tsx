066119ea93da7c18734c829a8755c5e90e6bf06e
Taejeong <mkiz0403@gmail.com>
2024-04-30 18:23:40 +0900

    Add : MainHeader.tsx

---
+++ client/src/shared/component/Header/MainHeader.tsx
@@ -0,0 +1,11 @@
+import './MainHeader.css';
+
+export default function MainHeader() {
+  return (
+    <>
+      <header className="main-header">
+        <h1>POKEMON</h1>
+      </header>
+    </>
+  );
+}
