f1c7021e9d6f1a82d11ee4192e01a615b7e2edd9
Taejeong <mkiz0403@gmail.com>
2024-05-11 16:03:42 +0900

    modify : pokemonDbApi.

--- client/src/apiServices/pokemonDbApi.ts
+++ client/src/apiServices/pokemonDbApi.ts
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import { PokemonInterface } from '../models/pokemons.Interface';
 
+// const apiUrl = 'http://127.0.0.1:4000/pokemons';
 const apiUrl = '54.180.106.202:4000/pokemons';
 const pokemonAxios = axios.create({ baseURL: apiUrl });
 
