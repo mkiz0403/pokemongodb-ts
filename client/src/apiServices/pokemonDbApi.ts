25c50c2d24d900e4ce93303d4b2a94a9cc591dd2
Taejeong <mkiz0403@gmail.com>
2024-05-14 15:05:52 +0900

    build : All

--- client/src/apiServices/pokemonDbApi.ts
+++ client/src/apiServices/pokemonDbApi.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { PokemonInterface } from '../models/pokemons.Interface';
 
 // const apiUrl = 'http://127.0.0.1:4000/pokemons';
-const apiUrl = '`54.180.133.19:4000/pokemons`';
+const apiUrl = '54.180.133.19:4000/pokemons';
 const pokemonAxios = axios.create({ baseURL: apiUrl });
 
 export async function getPokemons(): Promise<PokemonInterface[]> {
